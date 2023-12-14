c3a3c29edf06978370b3aa060b6f40c50d457f6a
Abhra <abhrabose000@gmail.com>
2023-12-14 13:45:01 +0530

    Password attribute is deleted

--- src/com/jsp/example/user.java
+++ src/com/jsp/example/user.java
@@ -1,5 +1,28 @@
 package com.jsp.example;
 
 class user {
+	private int userid;
+	private String name;
+	private String email;
+	
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 
 }
